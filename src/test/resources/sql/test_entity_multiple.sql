a59ccc64d9be4ca52277ad738f829eecd5a34603
Bernardo <bmg@envira.es>
2021-09-29 11:54:05 +0200

    Removed duplicated code

--- src/test/resources/sql/test_entity_multiple.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO example_entities (name) VALUES
-   ('entity_01'),
-   ('entity_02'),
-   ('entity_03'),
-   ('entity_04'),
-   ('entity_05');
--- src/test/resources/sql/test_entity_single.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO example_entities (name) VALUES
-   ('entity_01');
